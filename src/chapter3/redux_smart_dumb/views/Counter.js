8c37445b347f47a46378cee7d46949ac8f22ae94
xi-longg <xi-longg@hpe.com>
2017-08-11 16:08:35 +0800

    add redux_smart_dumb example and delete useless code in index.js

---
+++ src/chapter3/redux_smart_dumb/views/Counter.js
@@ -0,0 +1,78 @@
+import React,{Component,PropTypes} from 'react';
+import store from '../Store.js';
+import * as Actions from '../Actions';
+
+const buttonStyle={
+    margin:'10px'
+};
+
+class Counter extends Component{
+    render(){
+        const {caption,onIncrement,onDecrement,value}=this.props;
+        return(
+            <div>
+                <button style={buttonStyle} onClick={onIncrement}>+</button>
+                <button style={buttonStyle} onClick={onDecrement}>-</button>
+                <span>{caption} count:{value}</span>
+            </div>
+        );
+    }
+}
+
+Counter.propTypes={
+    caption:PropTypes.string.isRequired,
+    onIncrement:PropTypes.func.isRequired,
+    onDecrement:PropTypes.func.isRequired,
+    value:PropTypes.number.isRequired
+};
+
+class CounterContainer extends Component{
+    constructor(props){
+        super(props);
+        this.state=this.getOwnState();
+    }
+
+    getOwnState(){
+        return{
+            value:store.getState()[this.props.caption]
+        };
+    }
+
+    onIncrement=()=>{
+        store.dispatch(Actions.increment(this.props.caption));
+    }
+
+    onDecrement=()=>{
+        store.dispatch(Actions.decrement(this.props.caption));
+    }
+
+    onChange=()=>{
+        this.setState(this.getOwnState());
+    }
+
+    shouldComponentUpdate(nextProps,nextState){
+        return (nextProps.caption!==this.props.caption)||
+               (nextState.value!==this.state.value);
+    }
+
+    componentDidMount(){
+        store.subscribe(this.onChange);
+    }
+
+    componentWillUnmount(){
+        store.unsubscribe(this.onChange);
+    }
+
+    render(){
+        return <Counter caption={this.props.caption}
+          onIncrement={this.onIncrement}
+          onDecrement={this.onDecrement}
+          value={this.state.value} />
+    }
+}
+
+CounterContainer.propTypes={
+    caption:PropTypes.string.isRequired
+};
+
+export default CounterContainer;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
 import './index.css';
-import ControlPanel from './chapter3/redux_basic/views/ControlPanel';
+import ControlPanel from './chapter3/redux_smart_dumb/views/ControlPanel';
 
 ReactDOM.render(<ControlPanel />, document.getElementById('root'));
 
